101c3a6c1a2c7b3ce8d80167d4f1871a6aaacab2
VissentvNoord <49689172+VissentvNoord@users.noreply.github.com>
2023-11-21 19:36:24 +0100

    Rendering the gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import uniqid from "uniqid";
-
+import './styles/main.css'
 import Card from './components/card'
 
 function App() {
-  const [cardList, setCardList] = useState(Cards());
+  const [cardList, setCardList] = useState([]);
 
-  function ShuffleCards(){
-    const shuffledCards = [...cardList].sort(() => Math.random() - 0.5); 
+  useEffect(() => {
+    console.log("Use effect");
+    Cards(setCardList);
+  }, []);
+
+  useEffect(() => {
+    console.log("CardList updated:", cardList);
+  }, [cardList]);
+
+  function ShuffleCards() {
+    console.log(cardList);
+    const shuffledCards = [...cardList].sort(() => Math.random() - 0.5);
     setCardList(shuffledCards);
   }
 
@@ -17,21 +26,52 @@ function App() {
       <h1>Memory card</h1>
       <button onClick={ShuffleCards}>Shuffle</button>
 
-      {cardList.map((card) =>{
-        return <p id={card.id}>{card.id}</p>
-      })}
+      <div className='card-container'>
+        <DisplayCards cardList={cardList} />
+      </div>
     </>
   )
 }
 
-function Cards(){
+function DisplayCards({ cardList }) {
+  return (
+    <>
+      {cardList && cardList.length > 0 ? (
+        cardList.map((card) => (
+          <div className='card'>
+            {card.Visual()}
+          </div>
+        ))
+      ) : (
+        <p>Loading cards</p>
+      )}
+    </>
+  );
+}
+
+async function Cards(updateList) {
   let cards = [];
-  for(let i = 0; i < 12; i++){
-      const newCard = new Card(uniqid());
-      cards.push(newCard); 
+  for (let i = 0; i < 12; i++) {
+    const generatedGIF = await GenerateRandomGif();
+    const newCard = new Card(uniqid(), generatedGIF);
+    cards.push(newCard);
   }
   cards.sort(() => Math.random() - 0.5);
-  return cards;
+  updateList(cards);
+}
+
+async function GenerateRandomGif() {
+  const key = "4OyEIhRKI8sUnp4mS3Qk51iXgaedO7nc";
+
+  const apiLink = `https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=&rating=g`
+  return await fetch(apiLink)
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json.data.images.original.url);
+      return json.data.images.original.url;
+    })
 }
 
 export default App
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,27 @@
-function Card(_id){
+function Card(_id, _imgURL) {
     const id = _id;
+    let imgURL = _imgURL;
+    let checked = false;
 
-    return {id};
+    function Check() {
+        if (checked) {
+            return checked;
+        } else {
+            checked = true;
+            return false;
+        }
+    }
+
+    function Visual() {
+        return (
+            <div className="img-container">
+                <img src={imgURL}></img>
+            </div>
+        );
+    }
+
+    return { id, Visual, Check };
 }
+
 
 export default Card;
